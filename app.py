50a8d3d046904f2db97f531dd2e26c66782fb536
Arian Bortoli <arianbortoli@MacBook-Air-de-Arian.local>
2020-12-27 22:10:25 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+import plotly.express as px
+import pandas as pd
+
+
+workbook_url = 'data//5-INTER.xlsx'
+
+df = pd.read_excel(workbook_url, sheet_name = 'PASSES')
+
+df['Team'] = df['Team'].fillna('Não identificado')
+df['size'] = df["Passes per 90"]*df["Accurate passes, %"]/100
+
+fig = px.scatter(
+        df, x="Passes per 90", y="Accurate passes, %", color="Team", size='size', 
+        hover_data=['Player'])
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    dcc.Graph(id="scatter-plot",
+              figure = fig)
+])
+
+@app.callback(
+    Output("scatter-plot", "figure"))
+def update_bar_chart(slider_range):
+    
+    return fig
+
+app.run_server(debug=True)
